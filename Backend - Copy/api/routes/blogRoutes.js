f4eb1a8cba091d4c7421a68d75513a00e6dd5d71
Chetanrathore-13 <chetanrathore9131@gmail.com>
2025-04-15 15:56:29 +0530

    backend folder removed

--- Backend - Copy/api/routes/blogRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getBlogs, getBlogById } from "../controllers/blog.controller.js";
-
-const router = express.Router();
-
-router.get("/get_blogs", getBlogs);
-router.get("/get_blog/:slug", getBlogById);
-
-export default router;
